fc74b735d45f1beec77b3a0f31971f823f5862ce
caicindy87 <caicindy87@gmail.com>
2020-05-09 10:15:23 -0700

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 class Polygon {
-  constructor(array) {
-    this.sideLengths = [...array];
+  constructor(arrayOfSides) {
+    this.sideLengths = arrayOfSides;
   }
 
   get countSides() {
@@ -8,32 +8,35 @@ class Polygon {
   }
 
   get perimeter() {
+    if (!Array.isArray(this.sideLengths)) console.log("Not an array");
+
     return this.sideLengths.reduce((acc, side) => acc + side, 0);
   }
 }
 
 class Triangle extends Polygon {
   get isValid() {
+    if (!Array.isArray(this.sideLengths)) console.log("Not an array");
+    if (this.countSides !== 3) return console.log("Has to have 3 sides");
+
     const [a, b, c] = this.sideLengths;
-    if (a + b > c && b + c > a && a + c > b) {
-      return true;
-    } else {
-      return false;
-    }
+
+    // > operator returns boolean and && returns that boolean value, so don't need to write if statements
+    return a + b > c && b + c > a && a + c > b;
   }
 }
 
 class Square extends Polygon {
   get isValid() {
+    if (!Array.isArray(this.sideLengths)) console.log("Not an array");
+    if (this.countSides !== 4) return console.log("Has to have 4 sides");
     const [a, b, c, d] = this.sideLengths;
-    if (a === b && b === c && (c === d) & (d === a)) {
-      return true;
-    } else {
-      return false;
-    }
+    return a === b && b === c && c === d && d === a;
   }
 
   get area() {
+    if (!Array.isArray(this.sideLengths)) console.log("Not an array");
+    if (this.countSides !== 4) return console.log("Has to have 4 sides");
     const sideLength = this.sideLengths[0];
     return sideLength * sideLength;
   }
